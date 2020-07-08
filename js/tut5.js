598ddf1fa571e38eee77d868a686233aa05c5355
Tushar Ghadi <tusharghadi6710@gmail.com>
2020-07-08 19:47:03 +0530

    removed comment lines

--- js/tut5.js
+++ js/tut5.js
@@ -1,58 +1,58 @@
-// let arr = ['Tushar', 56, 'Virat', 78, true ];
-// console.log(arr)
-// console.log(arr.push('Santosh'))
-// console.log(arr)
-// console.log(arr.length)
-// console.log(arr.concat(7))
-// console.log(arr)
-// console.log(arr.reverse())
-//  let arr1 = [ 67,89,45,]
-//  arr = arr.concat(arr1)
-//  console.log(arr)
+let arr = ['Tushar', 56, 'Virat', 78, true ];
+console.log(arr)
+console.log(arr.push('Santosh'))
+console.log(arr)
+console.log(arr.length)
+console.log(arr.concat(7))
+console.log(arr)
+console.log(arr.reverse())
+ let arr1 = [ 67,89,45,]
+ arr = arr.concat(arr1)
+ console.log(arr)
 
-//  let obj ={ Name : 'Tushar',
-//             RollNo : 199317,
-//             Subject : 'MPA',
-//             marks : [45,67,89]}
+ let obj ={ Name : 'Tushar',
+            RollNo : 199317,
+            Subject : 'MPA',
+            marks : [45,67,89]}
 
-//             console.log(obj.Name)
-//             console.log(obj.marks)
+            console.log(obj.Name)
+            console.log(obj.marks)
 
-// Break and continue
+//Break and continue
 
-// let i=0;
-// for (i = 0; i < 5; i++) {
-//     if (i===3) {
-//         break;
-//     }
-//     console.log(i);
+let i=0;
+for (i = 0; i < 5; i++) {
+    if (i===3) {
+        break;
+    }
+    console.log(i);
     
-// }
-// console.log("end of break")
-// let j=0;
-// for (j = 0; j < 5; j++) {
-//     if (j===3) {
-//         continue;
-//     }
-//     console.log(j);
+}
+console.log("end of break")
+let j=0;
+for (j = 0; j < 5; j++) {
+    if (j===3) {
+        continue;
+    }
+    console.log(j);
     
-// }
-// console.log("end of continue")
+}
+console.log("end of continue")
 
-//traverding in array using for each loop
-// let array = ['tushar', 78,"hello", 67]
+//traversing in array using for each loop
+let array = ['tushar', 78,"hello", 67]
 
-// array.forEach(function(element) {
-//     console.log(element)
+array.forEach(function(element) {
+    console.log(element)
     
-// });
+});
 
-// // functions
-// function greet(name) {
+// functions
+function greet(name) {
 
-//     let greeting = `Good Morning , ${name}`
-//     return greeting
+    let greeting = `Good Morning , ${name}`
+    return greeting
     
-// }
-// let greeting = greet('Tushar')
-// console.log(greeting)
+}
+let greeting = greet('Tushar')
+console.log(greeting)
--- js/tut6.js
+++ js/tut6.js
@@ -1,27 +1,27 @@
-// let element = document.getElementById('myfirst')
+let element = document.getElementById('myfirst')
 
-// element = document.getElementsByTagName('div')
-// console.log(element)
+element = document.getElementsByTagName('div')
+console.log(element)
 
-// Array.from(element).forEach(element => {
-//     console.log(element)
-//     element.style.color = 'blue'
+Array.from(element).forEach(element => {
+    console.log(element)
+    element.style.color = 'blue'
     
-// });
+});
 
-// query selector
-// # - id   and  . - class
-// let sel = document.querySelector('#myfirst')
-// console.log(sel)
-// sel = document.querySelector('.child')
-// console.log(sel)
+//query selector
+//# - id   and  . - class
+let sel = document.querySelector('#myfirst')
+console.log(sel)
+sel = document.querySelector('.child')
+console.log(sel)
  
-// console.countReset()
+console.countReset()
 
-// let a = document.links
-// Array.from(a).forEach(element => {
-//     if(element.href.includes("deep")){
-//         console.log(element.href)
-//     }
+let a = document.links
+Array.from(a).forEach(element => {
+    if(element.href.includes("deep")){
+        console.log(element.href)
+    }
     
-// });
+});
