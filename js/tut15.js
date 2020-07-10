f33237f1b13445978c119ef8c1f972278d564392
Tushar Ghadi <tusharghadi6710@gmail.com>
2020-07-10 21:23:15 +0530

    tut15.js updated

--- js/tut15.js
+++ js/tut15.js
@@ -6,9 +6,48 @@ class Employee{
         this.experience = givenExperience
         this.division = givenDivision
     }
+
+    static slogan(){
+        return `I am ${name} and this company is best.`
+    }
+    joiningYear(){
+        return 2020 - this.experience
+    }
+    static add(a,b){
+        return a + b
+
+    }
 }
 
 
-let obj = new Employee("tushar",56,'B')
-console.log(obj.name)
-console.log(obj)
+let obj = new Employee("tushar",6,'B')
+// let slogan = obj.slogan()
+// console.log(slogan)
+//console.log(obj.name)
+//let experience = obj.joiningYear()
+// console.log(experience)
+// console.log(Employee.add(4,5))
+
+class Programmer extends Employee{
+    constructor(givenName,givenExperience,givenDivision,givenLanguage,github){
+        super(givenName,givenExperience,givenDivision)
+        this.language = givenLanguage
+        this.github = github
+    }
+     favoriteLanguage(){
+        if (this.language == 'python') {
+            return 'Python'
+        }
+        else{
+            return 'Javascript'
+        }
+    }
+    static multi(a,b){
+        return a*b
+    }
+}
+
+Tushar = new Programmer('Tushar',3,'Lays','Go','tushar382')
+console.log(Tushar)
+console.log(Programmer.multi(6,7))
+
